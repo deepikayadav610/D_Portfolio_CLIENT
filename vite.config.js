0569383a9a03bf5e97616615a1747cee9e3334e1
DEEPIKA YADAV <138503237+deepikayadav610@users.noreply.github.com>
2025-02-08 18:43:07 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0',  
+  },
+  preview: {
+    port: process.env.PORT || 5173,  
+    host: '0.0.0.0',
+  }
+});
